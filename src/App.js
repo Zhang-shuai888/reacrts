5ec6b826a2a0a52d83b9952cad782ba7c0a748ba
张帅 <2440868970@qq.com>
2020-09-22 21:06:00 +0800

    沙比

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { HashRouter  } from "react-router-dom"
+function App() {
+  return (
+    <HashRouter >
+
+    </HashRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                我是登录页面
+            </div>
+        );
+    }
+}
+
+export default Login;
